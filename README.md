47e147068ca37457df250de151de7d3fa2e907d6
3sho7mi8 <hiro.pinot@gmail.com>
2018-05-27 18:35:05 +0900

    Add scraping code

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Scraping Sample
+# Scraping Sample
+
+Mac:
+```
+alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
+
+chrome --headless --disable-gpu --その他のフラグ
+```
+
+参照:
+[Headless Chrome をさわってみた](http://www.cyokodog.net/blog/headless-chrome/)
+[RubyとSeleniumでHeadless chromeを動かす on Ubuntu/Linux](https://qiita.com/meguroman/items/41ca17e7dc66d6c88c07)
--- scra.rb
+++ scra.rb
@@ -1,1 +1,22 @@
 require 'bundler/setup'
+require 'capybara'
+require 'selenium-webdriver'
+
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app,
+    browser: :chrome,
+    desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(
+      chrome_options: {
+        args: %w(headless disable-gpu window-size=1280,800),
+      },
+    )
+  )
+end
+Capybara.javascript_driver = :selenium
+
+def start_scraping(url, &block)
+  Capybara::Session.new(:selenium).tap { |session|
+    session.visit url
+    session.instance_eval(&block)
+  }
+end
